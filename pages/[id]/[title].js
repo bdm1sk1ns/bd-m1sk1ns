3e7faa109b37dba7b72da7240a4a3db1baa286d1
bdm1sk1ns <80171018+bdm1sk1ns@users.noreply.github.com>
2021-03-08 02:56:46 +0600

    details added

--- pages/[id]/[title].js
+++ pages/[id]/[title].js
@@ -12,7 +12,14 @@ const CompanyPage = () =>{
             if (String(title).localeCompare(item.title) == 0){
                 return(
                     <Layout>
-                    <div>{item.title}</div>
+                        <div className="text-center">
+                            <section className="text-lg">{item.title}</section>
+                            <section className="px-8 lg:px-32 text-justify lg:text-center font-light text-base py-6 text-gray-900">{item.description}</section>
+                            <section className="pt-4 text-sm">Circular post</section>
+                            <section className="mx-auto pt-6 px-4">
+                                <img className="mx-auto" src={item.pocImg} />
+                            </section>
+                        </div>
                     </Layout>
                 )
             }
