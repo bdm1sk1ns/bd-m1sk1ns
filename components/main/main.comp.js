de4d9af3bd2c5152894294d6d30528874ee45748
bdm1sk1ns <80171018+bdm1sk1ns@users.noreply.github.com>
2021-03-08 02:54:15 +0600

    poc img added

--- components/main/main.comp.js
+++ components/main/main.comp.js
@@ -6,10 +6,11 @@ const ListOfCompanies = () => {
         <div className="text-center lg:px-4">
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-6">
                 {Companies.map((company) => (
-                    <div className="cursor-pointer">
+                    <div key={company.id} className="cursor-pointer">
                         <Link as={`/${company.id}/${company.title}`} href="/[id]/[title]">
                             <div className="pt-4 bg-gray-50 shadow-lg h-24 transform scale-100 hover:scale-105 transition duration-300 ease-in">
                                 <section className="tracking-widest text-lg font-extralight">{company.title}</section>
+                                <section className="tracking-wider text-xs text-blue-600 font-extralight">{company.shortDes}</section>
                             </div>
                         </Link>
                     </div>
