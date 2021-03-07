72f71473cf998cc3ada01f853692148d70f8defe
bdm1sk1ns <80171018+bdm1sk1ns@users.noreply.github.com>
2021-03-08 02:52:36 +0600

    added abouts and email

--- pages/about.js
+++ pages/about.js
@@ -5,7 +5,7 @@ const About = () => {
     return(
         <div className="flex flex-col h-screen bg-white">
             <Head>
-                <title>Bd-miskins</title>
+                <title>Bd-miskins: Miskin comapnies of Bangladesh</title>
             </Head>
             <div className="text-black">
                  <HeaderComp />
@@ -13,17 +13,19 @@ const About = () => {
             <main className="flex-1 overflow-y-auto lg:p-5 p-2">
                 <div className="text-center">
                     <section className="text-2xl font-light tracking-widest">Why?</section>
-                    <section className="text-justify font-light pt-6 px-6 lg:px-24">Many companies doesn't valuate their employees fully. Hell Most company doesn't valuate
-                        their employees properly. But a few companies crosses the HUMANE limit and makes the qualification and hard 
-                        work of the student seem like a joke. This website keeps a track of those shitty companies.
+                    <section className="text-justify font-light pt-6 px-6 lg:px-24">Most companies don't evaluate their employees properly. But a few companies cross the HUMANE limit and make the qualification and hard work of the student seem like a joke. This website keeps a track of those shitty companies.
                     </section>
                 </div>
             </main>
             <div className="text-center p-4">
+
                 <section className="pl-10 font-light">Creators</section>
                 <div className="text-sm lg:text-lg font-light">
-                    <section className="inline p-2">Idea >> <a href="#" className="font-normal">SIS</a></section>
-                    <section className="inline p-2">Code >> <a href="#" className="font-normal">Oni Laertes</a></section>
+                    <section className="inline p-2">Idea >> <a href="mailto:ssaappuurree@gmail.com" className="font-normal">Nahid</a></section>
+                    <section className="inline p-2">Code >> <a href="mailto:cozyozzy@tutanota.com" className="font-normal">Laertes</a></section>
+                </div>
+                <div>
+                  <section className="text-md font-light pb-4 pt-6">Want to enlist a company or have any complain? E-mail us<p style={{ color:"#1E3A8A"}} className="font-normal">ssaappuurree@gmail.com</p></section>
                 </div>
             </div>
         </div>
@@ -31,3 +33,4 @@ const About = () => {
 }
 
 export default About;
+
