3629e9710430cc3c9c89dbc4c60d929d2fc94ea9
bdm1sk1ns <80171018+bdm1sk1ns@users.noreply.github.com>
2021-03-08 02:53:29 +0600

    added about page

--- layout/layout.js
+++ layout/layout.js
@@ -4,25 +4,27 @@ import HeaderComp from '../components/header/header.comp';
 
 const Layout = (props) => {
     return(
-        <div className="flex flex-col h-screen bg-white">
-            <Head>
-                <title>Bd-miskins</title>
-            </Head>
-            <div className="inline text-black">
-                <HeaderComp />
+        <div style={{whiteSpace: "pre-line"}}>
+            <div className="flex flex-col h-screen bg-white">
+                <Head>
+                    <title>Bd-miskins: Miskin comapnies of Bangladesh</title>
+                </Head>
+                <div className="inline text-black">
+                    <HeaderComp />
+                </div>
+
+                <main className="flex-1 overflow-y-auto lg:p-5 p-2">
+                    {props.children}
+                </main>
+
+                <div className="text-right">
+                    <section className="mb-2 p-2 cursor-pointer" style={{ color: "#DB2777" }}>
+                        <Link href="/about">
+                            <span className="tracking-widest text-md lg:text-xl font-light p-3 rounded">What is this?</span>
+                        </Link>
+                    </section>
+                </div>
             </div>
-
-            <main className="flex-1 overflow-y-auto lg:p-5 p-2">
-                {props.children}
-            </main>
-
-            <footer className="text-right">
-                <section className="mb-2 p-2 cursor-pointer">
-                    <Link href="/about">
-                        <span className="tracking-widest text-md font-light text-red-200 p-3 rounded">What is this?</span>
-                    </Link>
-                </section>
-            </footer>
         </div>
     )
 }
