4a956efa7a5c944ab3e0bf19340cdd9ae5cdd39c
bdm1sk1ns <80171018+bdm1sk1ns@users.noreply.github.com>
2021-03-08 02:54:54 +0600

    comapany data added

--- components/main/main.data.js
+++ components/main/main.data.js
@@ -1,34 +1,44 @@
 export default [
     {
         id:"azxc",
-        title:"Company a",
+        title:"Hivecore",
+        shortDes:">Experienced Web or Embeded sytem developer\n>Salary: 5000tk.",
+        description:"Hivecore posted a job looking for CSE/CS and EEE graduates for internship offering a salary of 5000tk per month. They also asked for developer with *any kind of experience* in WEB DEVELOPMENT or EMBEDED SYSTEMS",
+        pocImg:"https://i.imgur.com/ZyAyl2L.png",
     },
     {
         id:"axhs",
-        title:"Comp b",
+        title:"Dreamploy",
+        shortDes:">CSE graduate\nNon-paid internship",
+        description:"Dreaploy Advertising and Freelancing(Pvt) Ltd. is looking for a CSE graduate who can work under pressure for 4 months in a non-paid internship. You up for it? Apply now.",
+        pocImg:"https://i.imgur.com/ICCMXTH.png",
     },
     {
         id:"azxcf",
-        title:"Company af",
+        title:"Link Tech IT",
+        shortDes:">Competetive programmer\nNon-paid internship",
+        description:"OOP, MVC, Experience in Laravel, Python, Django, Partcipation in Competetive programming and non-paid internship. Yep, non-paid. Link Tech IT really upped the bar I guess.",
+        pocImg:"https://i.imgur.com/5hWuf6O.png",
     },
     {
         id:"axhsf",
-        title:"Comp bf",
+        title:"Mr Khan IT",
+        shortDes:">C programmer\n>Non-paid internship",
+        description:"Responsibilities are web design and development and asking for good knowledge in C ??",
+        pocImg:"https://i.imgur.com/327lall.png",
     },
     {
         id:"azxcd",
-        title:"Company ad",
+        title:"Talha Soft",
+        shortDes:">CSE/EEE/SE/Diploma\nNon-paid internship for 3 months",
+        description:"CSE/EEE/SE/Diploma graduate, non-paid internship for 3 months. Must have own laptop? Why? Shouldn't the company provide it? Should an accountant must bring pen and paper too?",
+        pocImg:"https://i.imgur.com/uQZVNUo.png",
     },
     {
         id:"axhsd",
-        title:"Comp bd",
-    },
-    {
-        id:"azxcfd",
-        title:"Company afd",
-    },
-    {
-        id:"axhsfd",
-        title:"Comp bfd",
+        title:"Wezapps",
+        shortDes:">Freshers are encouraged to apply\nExperienced based salary",
+        description:"12k-15k salary but experienced based?",
+        pocImg:"https://i.imgur.com/bni4VF9.png",
     },
 ]
